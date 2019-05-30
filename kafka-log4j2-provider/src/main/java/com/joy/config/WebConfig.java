3f6c7da402c917505bb8907441d8da61728bedfe
liuhuijun <liuhuijun2017@gmail.com>
2019-05-30 18:06:26 +0800

    test kafka down.

---
+++ kafka-log4j2-provider/src/main/java/com/joy/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.joy.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Description:
+ * @Author: Joy
+ * @Date: 2019-05-30 17:55
+ */
+@EnableWebMvc
+@ComponentScan("com.joy.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
---
+++ kafka-log4j2-provider/src/main/java/com/joy/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.joy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Joy
+ * @Date: 2019-05-30 17:36
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    @GetMapping("/test")
+    public String test(){
+        kafkaTemplate.send("default-topic", "hello world");
+        return "ok";
+    }
+
+}
